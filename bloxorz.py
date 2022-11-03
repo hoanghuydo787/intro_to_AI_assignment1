1f0d2a7590580ed7f4be1e09cf8798b4d6d61d1e
Hoang <hoanghuydo787@gmail.com>
2022-11-03 21:47:52 +0700

    add time and mem usage

--- bloxorz.py
+++ bloxorz.py
@@ -1,9 +1,14 @@
 from sys import argv
 from bloxorz_logic import bloxorz_bfs, bloxorz_ga
+import time
+import psutil
 '''
 
 '''
 _, input, solver = argv
+
+start_time = time.time()
+mem_before = psutil.Process(os.getpid()).memory_info().rss
 
 if solver == 'DFS':
     test = bloxorz_bfs('input{}.txt'.format(input))
@@ -12,3 +17,8 @@ elif solver == 'GA':
     test = bloxorz_ga('input{}.txt'.format(input))
     test.GA_solver()  
 
+end_time = time.time()
+mem_after = psutil.Process(os.getpid()).memory_info().rss
+
+print("Time elapsed: ", start_time - end_time, "seconds")
+print("Memory usage: ", mem_after - mem_before, "MB")
